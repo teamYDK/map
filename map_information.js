5e17981fe41f535e22a7d85e4ab80b426f55c44a
yuki-maehara <31123693+yuki-maehara@users.noreply.github.com>
2017-08-28 18:22:07 +0900

    Add files via upload

--- map_information.js
+++ map_information.js
@@ -2,7 +2,7 @@ function initMap(){
 
 
 
-        var map = new google.maps.Map(document.getElementById('map'), {
+    var map = new google.maps.Map(document.getElementById('map'), {
 
 	center: {lat: 35.681, lng: 139.767},
 
@@ -233,56 +233,47 @@ function initMap(){
 
     var uluru = {lat: -25.363, lng: 131.044};
 
-    var contentString = '<div id="content">'+
-
-	'<div id="siteNotice">'+
-
+ /*   var contentString = 
+	'<!-- <div id="modal-overlay"></div> -->'+
+	'<div class="ex_2" style="left: 275.5px; top: 133.5px;">'+
+	'<!-- モーダルウインドウ -->'+
+	'<div id="modal-content">'+
+	'<form id="detail">'+
+	'<p id="username">UserName</p>'+
+	'<hr>'+
+	'<p id="comment">Comment</p>'+
+	'<hr>'+
+	'<p>Like　Comment</p>'+
+	'<hr>'+
+	'</form>'+   
+	'<img src="picture/Effel.JPG">'+   
+	'<p>'+
+	'<a id="modal-close" class="button-link">'+
+	'<p id="close">閉じる</p>'+
+	'</a>'+
+	'</p>'+
+	'<!-- モーダルウィンドウのコンテンツの終了 -->'+
 	'</div>'+
-
-	'<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-
-	'<div id="bodyContent">'+
-
-	'<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-
-	'sandstone rock formation in the southern part of the '+
-
-	'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-
-	'south west of the nearest large town, Alice Springs; 450&#160;km '+
-
-	'(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-
-	'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-
-	'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-
-	'Aboriginal people of the area. It has many springs, waterholes, '+
-
-	'rock caves and ancient paintings. Uluru is listed as a World '+
-
-	'Heritage Site.</p>'+
-
-	'<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-
-	'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-
-	'(last visited June 22, 2009).</p>'+
-
-	'</div>'+
-
-	'</div>';
+	'<p>'+
+	'<a id="modal-open" class="button-link"><img class="ipicture" src="picture/tapi.png"></a>'+
+	'</p>'+
+	'<!-- ここまでモーダルウィンドウ -->'+
+	'</div>'; */
 
 
+    
 
     var infowindow = new google.maps.InfoWindow({
+	
+	content:
+	'<div id= "img.ipicture">Username<br>Comment<br>'+
+	'<img scr="picture/Effel.JPG"><div>'
 
-	content: contentString
-
+	
     });
 
 
-
+    
     var marker = new google.maps.Marker({
 
 	position: uluru,
@@ -294,11 +285,10 @@ function initMap(){
     });
 
     marker.addListener('click', function() {
-
+	
 	infowindow.open(map, marker);
 
     });
-
 
 
 }
